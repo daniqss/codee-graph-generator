832828b26c8e42b8067b94638cb0e6a3f2467021
LucaDangeloS <lucad7@gmail.com>
2024-02-16 22:56:06 +0100

    Estructura básica

---
+++ server/read_data.py
@@ -0,0 +1,4 @@
+# def function read_data(csv_path)
+# devuelve un pandas DataFrame
+
+# 
---
+++ server/stats.py
@@ -0,0 +1,7 @@
+# def group_by_date(df: DataFrame) 
+
+# def group_by_hour(df)
+
+# def filter_by_date(from=None, to=None)
+
+# def 

dc86ca4d8f201e680e94d69dcdd3450f9f4eadbb
gaaelgarciia <gael.garciia@udc.es>
2024-02-16 23:45:51 +0100

    group by date and filter by date implemented in stats.py

--- server/stats.py
+++ server/stats.py
@@ -1,7 +1,24 @@
-# def group_by_date(df: DataFrame) 
+import pandas as pd
+
+def group_by_date(df: pd.DataFrame) :
+    df['Fecha'] = pd.to_datetime(df['Fecha'], dayfirst=True)
+    df_sorted = df.sort_values(by='Fecha')
+    result = df_sorted.groupby(df_sorted['Fecha'].dt.date)['Consumo_KWh'].sum()
+    return result
 
 # def group_by_hour(df)
 
-# def filter_by_date(from=None, to=None)
+def filter_by_date(df: pd.DataFrame, start_date=None, end_date=None) :
+    if start_date and end_date:
+        mask = (df['Fecha'] > start_date) & (df['Fecha'] <= end_date)
+        return df.loc[mask]
+    elif start_date:
+        mask = (df['Fecha'] > start_date)
+        return df.loc[mask]
+    elif end_date:
+        mask = (df['Fecha'] <= end_date)
+        return df.loc[mask]
+    else:
+        return df
 
 # def 

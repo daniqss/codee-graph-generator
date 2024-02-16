61fe0312b733582bd65616a9067d01592abefa41
ranicocs <danielqueijo14@gmail.com>
2024-02-17 00:46:34 +0100

    Update client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,72 @@
-<script setup>
-</script>
-
 <template>
-<h1>hola</h1>
+  <nav>
+    <a href="#">Home</a>
+    <a href="#">Try</a>
+    <a href="#">Contact</a>
+  </nav>
+
+  <main>
+    <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque aperiam fuga recusandae cupiditate facere voluptate, possimus veritatis asperiores minima laudantium ratione iusto excepturi blanditiis fugit esse numquam velit optio magnam.</h1>
+    <div>
+      <h1>Lista de Usuarios</h1>
+      <ul>
+        <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+      </ul>
+    </div>
+  </main>
 </template>
 
+<script>
+import axios from 'axios';
+  
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        this.users = response.data;
+      }
+      catch (error) {
+        console.error('Error al obtener usuarios:', error);
+      }
+    },
+  },
+};
+
+</script>
+
+
 <style scoped>
-h1 {
-  color: red;
+
+body {
+  background-color: var(--dark);
+  color: white;
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  background-color: var(--darker);
+  padding: 10px;
+}
+
+nav a {
+  color: var(--primary);
+  text-decoration: none;
+  margin-right: 15px;
+}
+
+main {
+  padding-left: 30%;
+  padding-right: 30%;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,6 @@
 import './assets/main.css'
+import 'virtual:uno.css'
+
 
 import { createApp } from 'vue'
 import App from './App.vue'

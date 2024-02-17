382f9fe8f7b5d903f7bfd36eaa5b5e1e6c14850b
gaaelgarciia <gael.garciia@udc.es>
2024-02-17 17:57:43 +0100

    Updated version of checkers with new type of chart

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 import read_data as rd
 from datetime import datetime as dt
 import stats.category as cat
-# import stats.checkers as chk
+#import stats.checkers as chk
 import stats.screening as scr
 from to_html import to_html
 # from send_to_email import send_to_email
--- stats/checkers.py
+++ stats/checkers.py
@@ -1,24 +1,15 @@
-import pandas as pd
-import json
-import numpy as np
 import matplotlib.pyplot as plt
-import read_data
+import seaborn as sns
+import pandas as pd
 
-def checker_LevelRate(df: pd.DataFrame):
-     cp_df = df.copy()
-     cp_df = cp_df.iloc[:-1]
-     level_counts = df["Level"].value_counts()
-     return level_counts
+# Only one type of char, but we should be able to use it for priority and #
 
-def checker_LRate(df: pd.DataFrame):
-     fig, ax = plt.subplots(1,2)
-     cp_df = df.copy()
-     cp_df = cp_df.iloc[:-1]
-     x = range(len(cp_df))
-     width = 0.3
-     ax.bar(x - width/3, cp_df["L1"], width, label ="L1")
-     ax.bar(x , cp_df["L2"], width, label ="L2")
-     ax.bar(x + width/3, cp_df["L3"], width, label = "L3")
+def bar_plot(df, col, title='Bar plot', include_total=True, max_range=0):
+    df = df.sort_values(by=[col, 'Checker'], ascending=[True, True])
 
-     ax.legend()
-     return fig
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    ax.bar(df['Checker'], df[col], color=plt.cm.Paired.colors)
+    ax.set_title(f"{title} by checker and {col}", pad=25)
+    ax.set_xticklabels(df['Checker'], rotation=75, fontsize=8)
+    return fig

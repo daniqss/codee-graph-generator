9c4b1c0146926d4e3dccf90b0f8e1dc6670618e7
LucaDangeloS <l.dangelo@udc.es>
2024-02-17 03:36:13 +0100

    Updated read_data typo

--- server/read_data.py
+++ server/read_data.py
@@ -2,18 +2,11 @@ import pandas as pd
 import json
 
 def read_data(json_path):
-    try:
-        file = json.load(open("dataset/example.json"))
-        screening = pd.DataFrame(file['Evaluation'][0])
-        category = pd.DataFrame(file['Evaluation'][1])
-        checkers = pd.DataFrame(file['Ranking of Checkers'][0])
+    file = json.load(open(json_path))
+    screening = pd.DataFrame(file['Evaluation'][0])
+    category = pd.DataFrame(file['Evaluation'][1])
+    checkers = pd.DataFrame(file['Ranking of Checkers'][0])
 
-        return screening, category, checkers
-
-    except FileNotFoundError: 
-        print(f"El archivo en la ruta '{json_path}' no se encontró. ")
-        return None 
-    except Exception as ex:
-        print(f"Ocurrio un error al leer el archivo CSV: {ex}")
-        return None        
+    return screening, category, checkers
+ 
 

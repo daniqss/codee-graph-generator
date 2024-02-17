35010d5b8464bc0aa963c6677b01d3c940e98417
LucaDangeloS <l.dangelo@udc.es>
2024-02-17 04:57:29 +0100

    Added type conversion into read_data

---
+++ server/constants.py
@@ -0,0 +1,29 @@
+
+
+TYPE_MAPPINGS_SCREENING = {
+    'Lines of code': int,
+    'Optimizable lines': int,
+    'Analysis time': int,
+    '# checks': int,
+    'Effort': int,
+    'Profiling': int,
+    'Cost': int,
+    'Profiling': float,
+}
+
+TYPE_MAPPINGS_CATEGORY = {
+    'Scalar': int,
+    'Control': int,
+    'Memory': int,
+    'Vector': int,
+    'Multi': float,
+    'Offload': float,
+    'Quality': int,
+    'L1': int,
+    'L2': int,
+    'L3': int,
+}
+
+TYPE_MAPPINGS_CHECKERS = {
+    '#': int, 
+}    

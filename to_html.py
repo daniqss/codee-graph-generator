22347aedd32e5eb77df7ad08b078b59b52a52812
ranicocs <danielqueijo14@gmail.com>
2024-02-17 15:09:50 +0100

    Change to_html params

--- to_html.py
+++ to_html.py
@@ -2,26 +2,35 @@ from datetime import datetime as dt
 import mpld3
 import os
 
-def figs_to_html (figs):
-    htmls = []
-    for fig in figs:
-        htmls.append(mpld3.fig_to_html(fig))
-    return htmls
+def figs_to_html (cat_figs, scr_figs, chk_figs):
+    cat_htmls = []
+    scr_htmls = []
+    chk_htmls = []
+    for fig in cat_figs:
+        cat_htmls.append(mpld3.fig_to_html(fig))
+    
+    for fig in scr_figs:
+        scr_htmls.append(mpld3.fig_to_html(fig))
+    
+    for fig in chk_figs:
+        chk_htmls.append(mpld3.fig_to_html(fig))
+    
+    return cat_htmls, scr_htmls, chk_htmls
 
-def to_html (filename, figs):
-    htmls = figs_to_html(figs)
+def to_html (filename, cat_figs, scr_figs, chk_figs):
+    cat_htmls, scr_htmls, chk_htmls = figs_to_html(cat_figs, scr_figs, chk_figs)
     
     combined_html = f"""
     <!DOCTYPE html>
     <html>
     <head>
-    <title>Mis Figuras</title>
+    <title>Figuras</title>
     <script src="https://d3js.org/d3.v5.min.js"></script>
     <script src="https://mpld3.github.io/js/mpld3.v0.5.5.js"></script>
     <style>
         body {{
             text-align: center;
-            margin: 20px; /* Ajusta el margen según tus preferencias */
+            margin: 30px; /* Ajusta el margen según tus preferencias */
         }}
         .figure-container {{
             display: inline-block;
@@ -31,13 +40,30 @@ def to_html (filename, figs):
             background-color: #f9f9f9; 
             padding: 10px; 
         }}
+        .titles {{
+            text-align: center;
+            font-family: 'Arial', sans-serif; /* Cambia 'Arial' por la fuente que prefieras */
+            color: #333030; /* Cambia el color del texto según tus preferencias */
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2); /* Añade sombra al texto */
+        }}
     </style>
     </head>
     <body>
+
+    <h1 class="titles">{filename}</h1>
     """
 
     # Agregar cada figura al HTML combinado
-    for html_fig in htmls:
+    combined_html += f'<h2 class="titles">Category</h2>\n'
+    for html_fig in cat_htmls:
+        combined_html += f'<div class="figure-container">{html_fig}</div>\n'
+
+    combined_html += f'<h2 class="titles">Screening</h2>\n'
+    for html_fig in scr_htmls:
+        combined_html += f'<div class="figure-container">{html_fig}</div>\n'
+
+    combined_html += f'<h2 class="titles">Checkers</h2>\n'
+    for html_fig in chk_htmls:
         combined_html += f'<div class="figure-container">{html_fig}</div>\n'
 
     combined_html += """
@@ -45,7 +71,7 @@ def to_html (filename, figs):
     </html>
     """
     os.makedirs('results', exist_ok=True)
-    with open(filename, 'w') as f:
+    with open("results/" + filename + ".html", 'w') as f:
         f.write(combined_html)
 
     return combined_html

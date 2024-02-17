4e375362d7e1399416859c5b62bc87716ef67509
gaaelgarciia <gael.garciia@udc.es>
2024-02-17 05:47:59 +0100

    updated category.py

--- server/stats/category.py
+++ server/stats/category.py
@@ -23,21 +23,24 @@ def category_rate(df: pd.DataFrame):
 
 def category_Lrate_graph(df: pd.DataFrame):
     L1_cache_rate, L2_cache_rate, L3_cache_rate = category_Lrate(df)
+
     L1_cache_rate.plot(kind='bar', color='r', alpha=0.7, label='L1')
     L2_cache_rate.plot(kind='bar', color='g', alpha=0.7, label='L2')
     L3_cache_rate.plot(kind='bar', color='b', alpha=0.7, label='L3')
-    plt.xlabel('')
-    plt.ylabel('%')
+
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1], ['0%', '20%', '40%', '60%', '80%', '100%'])
     plt.title('Gráfico de Barras')
     plt.legend()
     plt.show()
 
 def category_rate_graph(df: pd.DataFrame):
     Scalar_rate, Vector_rate, Memory_rate, Control_rate = category_rate(df)
+    
     Scalar_rate.plot(kind='bar', color='r', alpha=0.7, label='Scalar')
     Vector_rate.plot(kind='bar', color='g', alpha=0.7, label='Vector')
     Memory_rate.plot(kind='bar', color='b', alpha=0.7, label='Memory')
     Control_rate.plot(kind='bar', color='y', alpha=0.7, label='Control')
+
     plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1], ['0%', '20%', '40%', '60%', '80%', '100%'])
     plt.title('Gráfico de Barras')
     plt.legend()

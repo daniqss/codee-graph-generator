7e430b3dae5c9f1d01665c613313f8e26df10b7a
LucaDangeloS <l.dangelo@udc.es>
2024-02-17 04:11:01 +0100

    Updated read_data to read from string input

--- server/read_data.py
+++ server/read_data.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import json
 
-def read_data(json_path):
+def read_data(string):
+    file = json.loads(string)
+    screening = pd.DataFrame(file['Evaluation'][0])
+    category = pd.DataFrame(file['Evaluation'][1])
+    checkers = pd.DataFrame(file['Ranking of Checkers'][0])
+
+    return screening, category, checkers
+ 
+
+def read_data_from_file(json_path):
     file = json.load(open(json_path))
     screening = pd.DataFrame(file['Evaluation'][0])
     category = pd.DataFrame(file['Evaluation'][1])

b373cea5edb11ac00485afb1001cebf0fd6e62d2
LucaDangeloS <l.dangelo@udc.es>
2024-02-17 23:57:05 +0100

    Removed server.js

--- buildbot/master/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const port = process.env.PORT ?? 8080;
-const app = express();
-
-app.use(express.static('results'));
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});

9397d89028f50886cb9bd5fb4a73e1c6744d7ca7
ivanafonsoudc <ivan.afonso@udc.es>
2024-02-17 06:49:24 +0100

    checkers incomplete

--- server/stats/checkers.py
+++ server/stats/checkers.py
@@ -1,16 +1,22 @@
 import pandas as pd
-import json 
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import read_data
 
-def checkersTable(json_data): 
-    try: 
-        df = pd.DataFrame("../dataset/example.json")
-        return df 
-    except Exception as ex: 
-        print(f"Error al procesar el json {ex}")
-        return None 
-    
-with open("../dataset/example.json") as f:
-    json_data = json.load(f)
+def checker_LevelRate(df: pd.DataFrame):
+     cp_df = df.copy()
+     cp_df = cp_df.iloc[:-1]
+     level_counts = df["Level"].value_counts()
+     return level_counts
 
-df = checkersTable(json_data)
-print(df)
+def checker_LRate(df: pd.DataFrame):
+     fig, ax = plt.subplots(1,2)
+     cp_df = df.copy()
+     cp_df = cp_df.iloc[:-1]
+     ax[0].bar(cp_df.index, cp_df["L3"])
+     ax[0].set_title('Gráfico de Barras para L3')
+     ax[0].set_ylabel('Valor de L3')
+     ax[1].bar(cp_df.index, cp_df["L1"])
+     ax[1].set_title('Gráfico de Barras para L1')
+     ax[1].set_ylabel('Valor de L1')
